1e8a9b104404114969ded398858d2b984500aaea
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-11-15 15:25:28 -0800

    base template

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,116 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Ai } from '@cloudflare/ai'
+import OpenAI from 'openai';
+import cheerio from 'cheerio';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	// If you set another name in wrangler.toml as the value for 'binding',
+	// replace "AI" with the variable name you defined.
+	AI: any;
+	OPENAI_API_KEY: string;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env) {
+		try {
+			const body = JSON.parse(await request.text());
+			const { prompt, messages } = body;
+			const openai = new OpenAI({
+				apiKey: env.OPENAI_API_KEY
+			});
+
+			//cleaned messages
+			const cleanedMessages = messages.map((message: any) => {
+				return { content: message.content || "MISSING MESSAGE", role: message.role || "assistant" };
+			});
+			const chatCompletion = await openai.chat.completions.create({
+				messages: cleanedMessages,
+				model: 'gpt-4-1106-preview',
+				functions: [
+					{
+						name: "read_website_content",
+						description: "Read the content on a given website",
+						parameters: {
+							type: "object",
+							properties: {
+								url: {
+									type: "string",
+									description: "The URL to the website to read ",
+								}
+							},
+							required: ["url"],
+						},
+					}
+				]
+			});
+
+			const completionChoice = chatCompletion.choices[0];
+			const msg = completionChoice.message;
+
+			if (!msg) {
+				throw new Error("No message returned from OpenAI");
+			}
+
+			if (msg.function_call && msg.function_call.name === "read_website_content") {
+				console.log("REQUESTING WEBSITE CONTENT")
+				let websiteContent;
+				const url = JSON.parse(msg.function_call.arguments).url;
+				websiteContent = await read_website_content(url);
+				console.log("WEBSITE CONTENT", websiteContent)
+				const secondChatCompletion = await openai.chat.completions.create({
+					model: "gpt-4-1106-preview",
+					messages: [
+						...cleanedMessages,
+						{
+							role: "function",
+							name: msg.function_call.name,
+							content: websiteContent
+						}
+					],
+				});
+
+				const secondCompletionChoice = secondChatCompletion.choices[0];
+				const secondMSG = secondCompletionChoice.message;
+				const secondOutput = secondMSG.content;
+				return new Response(JSON.stringify(secondOutput));
+			}
+
+			const output = msg.content;
+			console.log({ output });
+			return new Response(JSON.stringify(output));
+		} catch (error) {
+			// Catch and return the error
+			console.log(error.message);
+			return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+		}
 	},
 };
+
+async function read_website_content(url) {
+	console.log("reading website content", url);
+
+	const response = await fetch(url);
+	const body = await response.text();
+	let cheerioBody = await cheerio.load(body);
+	const paragraphs = cheerioBody("p");
+	const spans = cheerioBody("span");
+	const headers = cheerioBody("h1, h2, h3, h4, h5, h6");
+	const links = cheerioBody("a");
+	const images = cheerioBody("img");
+	const lists = cheerioBody("ul, ol");
+	const tables = cheerioBody("table");
+	const forms = cheerioBody("form");
+	const scripts = cheerioBody("script");
+	const styles = cheerioBody("style");
+	const meta = cheerioBody("meta");
+	const title = cheerioBody("title");
+	const head = cheerioBody("head");
+
+	//add these to a single text string
+	const text = paragraphs.text() + spans.text() + headers.text() + links.text() + images.text() + lists.text() + tables.text() + forms.text() + scripts.text() + styles.text() + meta.text() + title.text() + head.text();
+	const resp = {
+		website_body: text,
+		url: url
+	}
+	return JSON.stringify(resp);
+}
+
